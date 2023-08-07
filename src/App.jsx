ea4f60542b349aef23f4bbca713bd4c87a56f028
CodePawsDev <isripen1991@outlook.com>
2023-08-07 17:35:48 +0700

    fix: fixing mobile landscape bg to be full

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import QRCode from "./assets/image-qr-code.png";
 function App() {
   
   return (
-    <div className="w-full h-screen bg-blue-100 flex justify-center items-center">
+    <div className="w-full min-h-screen bg-blue-100 flex justify-center items-center">
       <div className="w-340 sm:h-[550px] sm:w-[350px] bg-white rounded-xl flex flex-col items-center font-outfit text-center">
         <img src={QRCode} alt="QR Code" className="rounded-xl w-11/12 my-4"/>
         <h1 className="text-2xl font-bold w-10/12 text-gray-800 my-3">Improve your front-end skills by building projects</h1>
